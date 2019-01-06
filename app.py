89d8558715ad6ccd2e715d0cb7a775f136eed00a
Rokamudrik <rokamudrik@gmail.com>
2019-01-06 23:59:34 +0700

    changd the port to 5000

--- app.py
+++ app.py
@@ -48,5 +48,4 @@ def handle_text_message(event):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
